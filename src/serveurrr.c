ccc4150c7714e2fca53b629fa49b5c0c020b090b
tahanab <100781886+tahanab@users.noreply.github.com>
2022-05-20 06:54:38 +0000

    Create serveurrr.c

---
+++ src/serveurrr.c
@@ -0,0 +1,73 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[])
+{
+    writeFunc();
+}
+
+
+void writeFunc(void){
+   // La socket serveur
+    int listenfd = 0;
+    // La socket client (récupérée avec accept())
+    int connfd = 0;
+    // La structure avec les informations du serveur
+    struct sockaddr_in serv_addr = {0};
+    // Le buffer pour envoyer les données
+    char recvBuff[1025] = {0};
+    
+    // Création de la socket serveur
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    //Initialisation de la structure sockaddr
+    serv_addr.sin_family = AF_INET;
+    //Accepte les connexions depuis n'importe quelle adresse
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    // Le port sur lequel la socket va écouter
+    serv_addr.sin_port = htons(7000);
+    
+    // Association de la socket avec la structure sockaddr
+    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    
+    //La socket écoute pour des connexions
+    listen(listenfd, 10);
+    
+    // Récupération du nom de la machine
+    char hostname[128];
+    gethostname(hostname, sizeof hostname);
+    
+    int pid = 0;
+    
+        // Accepte la connexion d'une socket client
+        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+        
+        // Exécution d'un fork pour gérer la connexion
+       // if((pid=fork())==-1) {
+         //   printf("erreur\n");
+           // close(connfd);
+        //}
+        //else if(pid>0) { // Le processus père
+          //  close(connfd);
+        //}
+        //else if(pid==0) { // Le processus fils
+            bzero(recvBuff,1025);
+            recv(connfd,recvBuff,1025,0);
+            
+            printf("this is :%s\n",recvBuff);
+            FILE *out_file=fopen("list_client","a");
+            fprintf(out_file,"adress ip:%s",recvBuff);            
+            
+            close(connfd);
+            close(listenfd);
+        
+    
+}
